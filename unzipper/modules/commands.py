ce0ec633045d501c34761729e7d7eebcd7ac5f9a
Sanstar12 <94879410+Sanstar12@users.noreply.github.com>
2026-05-27 12:14:51 +0530

    Update commands.py

--- unzipper/modules/commands.py
+++ unzipper/modules/commands.py
@@ -47,14 +47,7 @@ https_url_regex = r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){
 
 
 def sufficient_disk_space(required_space):
-    disk_usage = psutil.disk_usage("/")
-    free_space = disk_usage.free
-    # Use a fixed buffer of 50MB instead of 5% of total>
-    # 5% on a 100GB+ drive is 5GB+, which is rarely ava>
-    buffer = 50 * 1024 * 1024
-    if free_space >= (required_space + buffer):
-        return True
-    return False
+    return True
 
 
 @unzipperbot.on_message(filters.private)
